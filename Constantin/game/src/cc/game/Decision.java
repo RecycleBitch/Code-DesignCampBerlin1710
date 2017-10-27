afc580288c2e4281c5f94a2d606ef2aeb454bea4
Constantin <you@example.com>
2017-10-27 10:45:50 +0200

    constantins projekt hinzugefuegt

---
+++ Constantin/game/src/cc/game/Decision.java
@@ -0,0 +1,30 @@
+package cc.game;
+
+import java.io.BufferedReader;
+
+public class Decision {
+	public boolean Answer_A = false;
+	public void getQuestion(){
+		
+		String question;
+		
+		question = "test a 10 66";
+		
+		String[] result = question.split("\\s");
+		System.out.println(question);
+	}
+
+	public void getAnswer(){
+		@SuppressWarnings("unused")
+		String Reanswer_A ;
+		Reanswer_A = "du bist super";
+		
+		//BufferedReader  reader = new BufferedReader(in);
+	 if(Answer_A = true){
+		 System.out.println(Reanswer_A);
+		 
+	; 
+	 }
+		
+	}
+}
